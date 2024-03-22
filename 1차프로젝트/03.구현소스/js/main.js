c03945b901da30eeba27481cf446356b19fe2fe8
Yanggang95 <aypd02150@gmail.com>
2024-03-22 18:17:31 +0900

    3.22

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -13,6 +13,42 @@ addEvt(window, "load", loadFn);
 
 function loadFn(){
 
+    //왼쪽버튼
+    const l_button = qs(".page-pagination .left");
+    //오른쪽버튼
+    const r_button = qs(".page-navigation .right");
+    //슬라이드
+    const slide = qs("#slide");
+
+    l_button.onclick = goSlide;
+    r_button.onclick = goSlide;
+
+
+
+    function goSlide(){
+
+        console.log("왔냐")
+        
+        let isRbtn = this.classList.contains('right');
+
+        if(isRbtn){
+            slide.style.left = '-100%';
+            slide.style.transition = '.6s ease-out';
+            
+            setTimeout(()=>{
+                slide.appendChild(
+                    slide.querySelectorAll('li')[0]);
+                    slide.style.left = '0';
+                    slide.style.transition = 'none';
+                },600)
+        }
+
+    }
+
+
+
+    
+    //family 버튼
     const f_button = qs(".fnb-family");
 
     f_button.onclick = (e) =>{
