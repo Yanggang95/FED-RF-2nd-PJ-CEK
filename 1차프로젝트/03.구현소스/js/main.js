e87191f8648944959227b1d1d0a6a5400ffb99c8
Yanggang95 <aypd02150@gmail.com>
2024-03-28 18:18:48 +0900

    3.28

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -9,9 +9,7 @@ const addEvt = (ele, evt, fn) => ele.addEventListener(evt, fn);
 
 addEvt(window, "load", loadFn);
 
-
-function loadFn(){
-
+function loadFn() {
     //왼쪽버튼
     const l_button = qs(".page-pagination .left");
     //오른쪽버튼
@@ -19,63 +17,96 @@ function loadFn(){
     //슬라이드
     const slide = qs("#slide");
 
-    for(let i=0;i<14;i++){
-        slide.innerHTML +=`<li>
-        <img src="./images/thumbnail${i+1}.jpg" alt="썸네일${i+1}">
-        </li> `
+    for (let i = 0; i < 14; i++) {
+        slide.innerHTML += `<li data-seq="${i}">
+        <img src="./images/thumbnail${i + 1}.jpg" alt="썸네일${i + 1}">
+        </li> `;
     }
-
 
     l_button.onclick = goSlide;
     r_button.onclick = goSlide;
 
     let prot = false;
 
+    function goSlide(evt, sts = true) {
+        // sts = true: 버튼클릭 / false : 자동호출
 
-    function goSlide(){
+        if (sts) {
+            clearAuto();
+        }
 
-        if(prot) return;
+        if (prot) return;
         prot = true;
         setTimeout(() => {
             prot = false;
         }, 600);
-        
-        let isRbtn = this.classList.contains('right');
 
-        if(isRbtn){
-            slide.style.left = '-100%';
-            slide.style.transition = '.6s ease-out';
-            
-            setTimeout(()=>{
-                slide.appendChild(
-                    slide.querySelectorAll('li')[0]);
-                    slide.style.left = '0';
-                    slide.style.transition = 'none';
-                },600)
-        }else{
-            let list = slide.querySelectorAll('li');
-            slide.insertBefore(list[list.length - 1], list[0]);
+        let isRbtn = sts ? this.classList.contains("right") : true;
 
-            slide.style.left = '-100%';
-            slide.style.transition = 'none';
+        if (isRbtn) {
+            slide.style.left = "-100%";
+            slide.style.transition = ".6s ease-out";
 
             setTimeout(() => {
-                slide.style.left = '0';
-                slide.style.transition = '.6s ease-out';
+                slide.appendChild(slide.querySelectorAll("li")[0]);
+                slide.style.left = "0";
+                slide.style.transition = "none";
+            }, 600);
+        } else {
+            let list = slide.querySelectorAll("li");
+            slide.insertBefore(list[list.length - 1], list[0]);
+
+            slide.style.left = "-100%";
+            slide.style.transition = "none";
+
+            setTimeout(() => {
+                slide.style.left = "0";
+                slide.style.transition = ".6s ease-out";
             }, 0);
         }
 
+        let seq = slide
+            .querySelectorAll("li")
+            [isRbtn ? 1 : 0].getAttribute("data-seq");
+
+        console.log(seq);
+
+        let ppc = qs('.page-pagination-current');
+
+        for(let i=0; i<14; i++){
+            if(i == seq){
+                ppc.innerHTML = '0'+(i+1);
+            }
+        }
+
+
+    }
+    let autoI;
+
+    let autoT;
+
+    autoSlide();
+
+    function autoSlide() {
+        autoI = setInterval(() => {
+            goSlide(false, false);
+        }, 3000);
+    }
+
+    function clearAuto() {
+        clearInterval(autoI);
+        clearTimeout(autoT);
+        autoT = setTimeout(() => {
+            autoSlide();
+        }, 5000);
     }
 
     //family 버튼
-    const fnb_family = qs(".fnb-family");
-    const p = qs(".fnb-family p")
+    const list = qs(".list-box");
+    const fam = qs(".fam");
 
-    p.onclick = (e) =>{
-        fnb_family.style.top = "-38%";
-        fnb_family.style.height = "42%";
-    }
-
+    fam.onclick = (e) => {
+        list.style.top = "-42%";
+        list.style.height = "42%";
+    };
 }
-
-
